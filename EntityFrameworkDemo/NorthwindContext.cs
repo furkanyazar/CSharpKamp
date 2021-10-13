f5d912091f5b55937a44a93bb091f795459e9350
Furkan Yazar <frknyz1@outlook.com>
2021-10-13 19:18:01 +0300

    updated

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.HasDefaultSchema("dbo");
+            modelBuilder.Entity<Personel>().ToTable("Employees");
+
+            modelBuilder.Entity<Personel>().Property(p => p.Id).HasColumnName("EmployeeID");
+            modelBuilder.Entity<Personel>().Property(p => p.Name).HasColumnName("FirstName");
+            modelBuilder.Entity<Personel>().Property(p => p.Surname).HasColumnName("LastName");
+        }
+    }
+}
